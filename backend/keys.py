c8e7683cd8fd94a5e6a31c2b6a079de5a1dd138f
raita0100 <raita0100@gmail.com>
2020-08-26 15:31:07 +0530

    added key file

---
+++ backend/keys.py
@@ -0,0 +1,4 @@
+#__keywords__ = ["data scientist fresher", "ui developer"]
+#__locations__ = ["Bengaluru, Karnataka", "Hyderabad, Telangana"]
+__keywords__ = ["ui developer"]
+__locations__ = ["Hyderabad, Telangana"]
